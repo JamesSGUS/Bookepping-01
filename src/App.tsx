d0aea6bef13e851a97246c9aa9fcdba824aee2ff
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-09 09:35:59 +0800

    setting navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,84 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.33%;
+      padding: 16px;
+      text-align: center;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <button>Hello world</button>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags"> <Tags/> </Route>
+            <Route path="/money"><Money/></Route>
+            <Route path="/statistics"><Statistics/></Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">Tags</Link>
+            </li>
+            <li>
+              <Link to="/money">Money</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Statistics</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
 }
 
+function NoMatch() {
+  return (
+    <div>Wrong Url input</div>
+  );
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
 export default App;
+
