fdc3a25d483783d1ba6733cc74cff778bc59b43b
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-12 11:19:05 +0800

    revised webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -374,7 +374,12 @@ module.exports = function (webpackEnv) {
                 {
                   loader: 'svgo-loader', options: {
                     plugins: [
-                      {removeAttrs: {attrs: 'fill'}}
+                      {
+                        name: 'removeAttrs',
+                        params: {
+                          attrs: '(fill|stroke)'
+                        }
+                      }
                     ]
                   }
                 },
