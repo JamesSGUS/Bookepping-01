945a2b69b4a09c1eabd277a5c8f79c0cb80a7558
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-12 21:55:59 +0800

    Page money's UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,17 @@ import Tags from './pages/Tags';
 import Money from './pages/Money';
 import Statistics from './pages/Statistics';
 import NoMatch from './pages/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+color: #333;
+`
 
 function App() {
   return (
+    <AppWrapper>
     <Router>
+
       <Switch>
         <Route path="/tags"> <Tags/> </Route>
         <Route path="/money"><Money/></Route>
@@ -23,6 +30,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </AppWrapper>
   );
 }
 
