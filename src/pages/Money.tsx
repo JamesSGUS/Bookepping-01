b7ffe68d8faf1db182de55df77a14c8b8c7d3d47
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-13 10:04:09 +0800

    layout area of category

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -6,19 +6,22 @@ const TagsSection = styled.section`
   background: #FFFFFF;
   border: 1px solid red;
   padding: 12px 16px;
-  >ol{
+
+  > ol {
     margin: 0 -12px;
+
     > li {
       background: #D9D9D9;
       border-radius: 18px;
-      display:inline-block;
+      display: inline-block;
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
     }
   }
-  >button {
-    background:none;
+
+  > button {
+    background: none;
     border: none;
     padding: 2px 5px;
     color: #666;
@@ -27,25 +30,50 @@ const TagsSection = styled.section`
   }
 `;
 const NotesSection = styled.section`
-background: #f5f5f5;
+  background: #f5f5f5;
   padding: 0 16px;
   font-size: 15px;
+
   > label {
     display: flex;
     align-items: center;
     white-space: nowrap;
-    >span {margin-right: 16px;}
-    >input {
+
+    > span {
+      margin-right: 16px;
+    }
+
+    > input {
       display: block;
       width: 100%;
       height: 72px;
-      background:none;
+      background: none;
       border: none;
     }
   }
 `;
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      padding: 16px 0;
+      text-align: center;
+      position: relative;
+      &.selected::after {
+        content: "";
+        display: block;
+        position: absolute;
+        height: 3px;
+        background: #333;
+        width: 100%;
+        bottom: 0;
+        left: 0;        
+      }
+    }
+  }
 `;
 const NumberPadSection = styled.section`
 
@@ -71,7 +99,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
