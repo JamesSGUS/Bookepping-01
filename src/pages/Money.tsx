98e29cc8e6caf73085aef5afee53c6e6e35a4f28
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-13 09:40:48 +0800

    layout area of notes

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -27,7 +27,22 @@ const TagsSection = styled.section`
   }
 `;
 const NotesSection = styled.section`
-
+background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 15px;
+  > label {
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    >span {margin-right: 16px;}
+    >input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background:none;
+      border: none;
+    }
+  }
 `;
 const CategorySection = styled.section`
 
@@ -51,7 +66,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="input here"/>
         </label>
       </NotesSection>
       <CategorySection>
@@ -78,7 +93,6 @@ function Money() {
           <button>0</button>
           <button>.</button>
         </div>
-
       </NumberPadSection>
     </Layout>
   );
