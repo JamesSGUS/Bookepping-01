6ea00195711aeb5be324b65b2ada34095d3fe54f
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-13 11:28:26 +0800

    layout area of nomberpad

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -54,14 +54,17 @@ const NotesSection = styled.section`
 `;
 const CategorySection = styled.section`
   font-size: 24px;
+
   > ul {
     display: flex;
     background: #c4c4c4;
+
     > li {
       width: 50%;
       padding: 16px 0;
       text-align: center;
       position: relative;
+
       &.selected::after {
         content: "";
         display: block;
@@ -70,13 +73,69 @@ const CategorySection = styled.section`
         background: #333;
         width: 100%;
         bottom: 0;
-        left: 0;        
+        left: 0;
       }
     }
   }
 `;
 const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
 
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -2px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -2px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+      &:nth-child(12){
+        background: #9a9a9a;      
+      }
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+  }
 `;
 
 function Money() {
@@ -104,21 +163,21 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
-          <button>deleted</button>
+          <button>Deleted</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
-          <button>clear</button>
+          <button>Clear</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>ok</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
