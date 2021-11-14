b9d92e2247b79e274d67f0850d73a9fa6a7900ad
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-14 20:54:27 +0800

    Adding function of tags

--- src/pages/Money/TagsSection.tsx
+++ src/pages/Money/TagsSection.tsx
@@ -1,3 +1,5 @@
+// @ts-ignore
+
 import styled from 'styled-components';
 import React, {useState} from 'react';
 
@@ -20,6 +22,10 @@ const Wrapper = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+
+      &.selected {
+        background: #f60;
+      }
     }
   }
 
@@ -33,19 +39,37 @@ const Wrapper = styled.section`
   }
 `;
 
-
-const TagsSection: React.FC = (props:any) => {
-  let initialState;
-  const [tags, setTags] = useState(['衣', '食', '住', '行']);
+const TagsSection: React.FC = (props: any) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>(['']);
+  const addTag = () => {
+    let message;
+    const tagName = window.prompt('New tag is:');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
   return (
     <Wrapper>
       <ol>
         {tags.map(tag =>
-          <li key={tag}>{tag}</li>
+          <li key={tag} onClick={
+            () => { onToggleTag(tag); }
+          } className={getClass(tag)}>{tag}</li>
         )}
       </ol>
-      <button>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
   );
 };
+
 export {TagsSection};
