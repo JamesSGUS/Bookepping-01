64fa012396f4a2a65c98152c6d65697fb0bf2201
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-15 11:48:37 +0800

    error 001

--- src/pages/Money/NumberPadSection.ts
+++ src/pages/Money/NumberPadSection.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
 
@@ -66,4 +67,35 @@ const NumberPadSection = styled.section`
       }
     }
 `;
+
+const NumberPadSection: React.FC = ()=>{
+  const [output, setOutput] = useState('0');
+  const onClickButtonWrapper = (e: React.MouseEvent)=>{
+    const text = (e.target as HTMLButtonElement).textContent
+        console.log(text);
+  };
+  return (
+    <Wrapper>
+      <div className="output">
+      {output}
+      </div>
+      <div className="pad clearfix" onClick={onClickButtonWrapper}>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>删除</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>清空</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button className="ok">OK</button>
+    <button className="zero">0</button>
+    <button className="dot">.</button>
+  </div>
+  </Wrapper>
+  );
+};
 export {NumberPadSection};
