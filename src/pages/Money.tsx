688b6930b2763afeef99eb3e375115b77f424186
Yinyong001 <YIN_YONG01.PT@ICHAT.SP.EDU.SG>
2021-11-20 17:15:33 +0800

    Arrangement of layout

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -19,6 +19,9 @@ const defaultFormData = {
   category: '-' as Category,
   amount: 0 as number
 };
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
 
 function Money() {
   const [selected, setSelected] = useState(defaultFormData);
@@ -39,8 +42,10 @@ function Money() {
                    onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
-      <CategorySection value={selected.category}
-                       onChange={category => onChange({category})}/>
+      <CategoryWrapper>
+        <CategorySection value={selected.category}
+                         onChange={category => onChange({category})}/>
+      </CategoryWrapper>
       <NumberPadSection value={selected.amount.toString()}
                         onChange={value => onChange({amount: parseFloat(value)})}
                         onOk={submit}/>
